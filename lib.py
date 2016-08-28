aafad6186f4dd9ad47fe2914c486c6c078df6f2c
pzia <paparazzia@gmail.com>
2016-08-28 13:54:00 +0200

    save logs in logs, not in tmp

--- lib.py
+++ lib.py
@@ -56,7 +56,7 @@ def clean(newtitle):
     return " ".join(words)
     
 def save_words():
-    with open('tmp/words.json.log', 'w') as outfile:
+    with open('logs/words.json.log', 'w') as outfile:
         json.dump(words_cleaned, outfile, sort_keys = True, indent = 4, ensure_ascii=False)
 
 def rss_extract(url, root = False):
